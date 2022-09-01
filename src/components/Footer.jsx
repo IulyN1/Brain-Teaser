3aa212c12ab76a8a13dd2919a7a7a5f227e10992
Popa Iulian-Alexandru <iulianp14@yahoo.com>
2022-09-01 23:57:16 +0300

    Added landing page and configured it for mobile also

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './styles/Footer.css';
+
+function Footer() {
+	return (
+		<div className="footer">
+			<img src="./images/logo.png" alt="logo" className="logoFooter"></img>
+			<p>Brain Teaser &copy; All rights reserved 2022-2023</p>
+		</div>
+	);
+}
+
+export default Footer;
